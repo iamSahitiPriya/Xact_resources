a0096ba848753bd931a7b62931fc766245654304
Shashank Mishra10 <smishra10@lenovo.com>
2022-03-16 17:37:47 +0530

    [NO-CARD] [Shashank/NA] | Update Java to 11 and install Git

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -5,7 +5,9 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade
-sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo yum install git -y
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
 sudo systemctl status jenkins
--- vars/template.tfvars
+++ vars/template.tfvars
@@ -2,6 +2,6 @@ aws_region = "ap-south-1"
 
 vpc_id = ""
 
-cidr_block = ""
+cidr_block = "0.0.0.0/0"
 
 key_name = "xact-jenkins-master"
