a3ac8a63e073a6ce32ace769bb0446a63e466006
Shashank Mishra10 <smishra10@lenovo.com>
2022-04-04 20:04:31 +0530

    [21] [Shashank/Ruchika] | Add ECS config for production

---
+++ ecs-prod/ecs_aws.tfvars
@@ -0,0 +1,14 @@
+aws_region = "ap-south-1"
+
+vpc_id = "vpc-09fe503df0ac56522"
+
+cidr_block = "0.0.0.0/0"
+
+environment = "prod"
+
+account = "730911736748"
+
+DB_USER = "dev/dbuser-h24eWj:PROD_DB_USER"
+
+DB_PWD ="dev/dbpwd-0wwWre:PROD_DB_PWD"
+
---
+++ ecs-prod/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  default     = "ap-south-1"
+}
+
+variable "vpc_id" {
+  description = "VPC for ECS"
+}
+
+variable "cidr_block" {
+  description = "CIDR Block to allow ECS Access"
+}
+
+variable "environment" {
+  description = "Service environment"
+}
+
+variable "account" {
+  description = "Account number"
+}
+
+variable "DB_USER" {
+  description = "ssm path of param"
+}
+
+variable "DB_PWD" {
+  description = "ssm path of param"
+}
