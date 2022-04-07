413ccd8b9d7c7186c4d6ea1ecbe2133ef76ec5bb
Shashank Mishra10 <smishra10@lenovo.com>
2022-04-07 12:24:37 +0530

    [26] [Shashank/Ram] | Change folder structure to organise env

--- Common/Jenkins/install_jenkins.sh
+++ Common/Jenkins/install_jenkins.sh
@@ -9,7 +9,9 @@ sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo yum install git -y
 sudo yum install -y xorg-x11-server-Xvfb gtk2-devel gtk3-devel libnotify-devel GConf2 nss libXScrnSaver alsa-lib
+sudo amazon-linux-extras install docker
 sudo amazon-linux-extras enable docker
+sudo service docker start
 sudo yum install amazon-ecr-credential-helper -y
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
