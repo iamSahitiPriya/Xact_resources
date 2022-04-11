44682bd062de0a0ee80d84314f6d4f20488ff8ee
Shashank Mishra10 <smishra10@lenovo.com>
2022-04-11 12:54:21 +0530

    [23] [Shashank] | Refactor to remove unused code & tag env with names

--- Dev-Env/S3/variable.tf
+++ Dev-Env/S3/variable.tf
@@ -4,12 +4,6 @@ variable "bucket_name" {
 variable "acl_value" {
   default = "private"
 }
-variable "aws_access_key" {
-  default = ""
-}
-variable "aws_secret_key" {
-  default = ""
-}
 variable "aws_region" {
   default = "ap-south-1"
 }
