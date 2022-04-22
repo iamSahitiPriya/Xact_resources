ceca8e5193d5a2a102555156231cd7253d3779aa
Shashank Mishra10 <smishra10@lenovo.com>
2022-04-22 14:26:15 +0530

    [No-card] [Shashank] | Add sonarqube

---
+++ Common/Sonar/install_sonar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo yum update –y
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo adduser --system --no-create-home --group --disabled-login sonarqube
+sudo mkdir /opt/sonarqube
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
+sudo chown -R sonarqube:sonarqube /opt/sonarqube
