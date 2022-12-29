61a757dc73f3db92d504ae1e28106826a9ca9546
rampg23 <ramprakash.pg@thoughtworks.com>
2022-12-29 15:55:58 +0530

    [226][Ram] | Refactor post migration cleanup configuration

--- migrate-db-to-non-prod.sh
+++ migrate-db-to-non-prod.sh
@@ -13,7 +13,7 @@ echo "Creating Instance from snapshot - ${SNAPSHOT_ID}"
 aws rds restore-db-instance-from-db-snapshot --db-instance-identifier ${TEMP_PROD_INSTANCE_NAME} --db-snapshot-identifier ${SNAPSHOT_ID}  --vpc-security-group-ids sg-0c4805d53deaceac9 --no-publicly-accessible
 echo "Instance Created"
 INSTANCE_STATUS=$(aws rds describe-db-instances --db-instance-identifier ${TEMP_PROD_INSTANCE_NAME} --query DBInstances[0].DBInstanceStatus)
-while [ $((INSTANCE_STATUS != AVAILABLE_STATUS)) ];
+while [ $INSTANCE_STATUS != $AVAILABLE_STATUS ];
 do
   echo "Waiting on Instance to be available - ${INSTANCE_STATUS}"
   sleep 10
@@ -23,7 +23,7 @@ echo "Modifying Instance credentials"
 aws rds modify-db-instance --db-instance-identifier temp-prod-instance --master-user-password ${TEMP_PROD_PASSWORD}
 
 INSTANCE_STATUS=$(aws rds describe-db-instances --db-instance-identifier ${TEMP_PROD_INSTANCE_NAME} --query DBInstances[0].DBInstanceStatus)
-while [ $((INSTANCE_STATUS != AVAILABLE_STATUS)) ];
+while [ $INSTANCE_STATUS != $AVAILABLE_STATUS ];
 do
   echo "Waiting on Instance to be available - ${INSTANCE_STATUS}"
   sleep 10
@@ -52,3 +52,4 @@ echo "Rename dev1 and qa1"
 psql --dbname=postgresql://"${NON_PROD_USERNAME}":${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactqa -c "ALTER DATABASE xactdev1 RENAME TO xactdev;"
 psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev -c "ALTER DATABASE xactqa1 RENAME TO xactqa;"
 
+while [ $((INSTANCE_STATUS!="backing-up")) ];do echo $INSTANCE_STATUS; INSTANCE_STATUS=$(aws rds describe-db-instances --db-instance-identifier temp-prod-instance --query DBInstances[0].DBInstanceStatus); sleep 10; done
--- post-migration-cleanup.sh
+++ post-migration-cleanup.sh
@@ -1,6 +1,6 @@
 TEMP_PROD_INSTANCE_NAME=temp-prod-instance
 DELETE_STATUS=$(aws rds describe-db-instances --db-instance-identifier ${TEMP_PROD_INSTANCE_NAME} --query DBInstances[0].DBInstanceStatus)
-while [ $((DELETE_STATUS != null)) ];
+while [ $DELETE_STATUS != null ];
 do
   sleep 10
   aws rds delete-db-instance --db-instance-identifier temp-prod-instance --delete-automated-backups --skip-final-snapshot
