74b00bc3eeee8362ce7dd3f7c87f32f3f65fc201
rampg23 <ramprakash.pg@thoughtworks.com>
2023-01-02 13:16:44 +0530

    [226][Ram|Shashank|Ruchika] | Create temp prod database before dumping.

--- migrate-db-to-non-prod.sh
+++ migrate-db-to-non-prod.sh
@@ -4,6 +4,7 @@ TEMP_PROD_PASSWORD=$(aws secretsmanager get-secret-value --secret-id migration/d
 NON_PROD_USERNAME=$(aws secretsmanager get-secret-value --secret-id non-prod/db --output text --query SecretString | grep -o '"username":"[^"]*' | grep -o '[^"]*$')
 NON_PROD_PASSWORD=$(aws secretsmanager get-secret-value --secret-id non-prod/db --output text --query SecretString | grep -o '"password":"[^"]*' | grep -o '[^"]*$')
 TEMP_PROD_INSTANCE_NAME=temp-prod-instance
+TEMP_PROD_DB=xactprodtemp
 PROD_DB=xactprod
 SNAPSHOT_ID=$1
 TEMP_NON_PROD_INSTANCE_NAME=temp-non-prod-instance
@@ -48,17 +49,23 @@ done
 NON_PROD_HOST=$(aws rds describe-db-instances --db-instance-identifier temp-non-prod-instance --query DBInstances[0].Endpoint.Address | tr -d '"')
 TEMP_PROD_HOST=$(aws rds describe-db-instances --db-instance-identifier temp-prod-instance --query DBInstances[0].Endpoint.Address | tr -d '"')
 
-echo "Copying prod instance to dev"
-pg_dump -C --dbname=postgresql://${TEMP_PROD_USERNAME}:${TEMP_PROD_PASSWORD}@${TEMP_PROD_HOST}:5432/${PROD_DB} | psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev
+echo "Creating temp-prod database"
+psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev -c "CREATE DATABASE ${TEMP_PROD_DB}"
 
-echo "Renaming prod to dev1"
-psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev -c "ALTER DATABASE xactprod RENAME TO xactdev1;"
+echo "Copying temp prod instance to dev"
+pg_dump -C --dbname=postgresql://${TEMP_PROD_USERNAME}:${TEMP_PROD_PASSWORD}@${TEMP_PROD_HOST}:5432/${PROD_DB} | psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/${TEMP_PROD_DB}
+
+echo "Renaming temp-prod to dev1"
+psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev -c "ALTER DATABASE ${TEMP_PROD_DB} RENAME TO xactdev1;"
+
+echo "Creating temp-prod database"
+psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev -c "CREATE DATABASE ${TEMP_PROD_DB}"
 
 echo "Copying prod instance to qa"
-pg_dump -C --dbname=postgresql://${TEMP_PROD_USERNAME}:${TEMP_PROD_PASSWORD}@${TEMP_PROD_HOST}:5432/${PROD_DB} | psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactqa
+pg_dump -C --dbname=postgresql://${TEMP_PROD_USERNAME}:${TEMP_PROD_PASSWORD}@${TEMP_PROD_HOST}:5432/${PROD_DB} | psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/${TEMP_PROD_DB}
 
 echo "Renaming prod to qa1"
-psql --dbname=postgresql://"${NON_PROD_USERNAME}":"${NON_PROD_PASSWORD}"@${NON_PROD_HOST}:5432/xactdev -c "ALTER DATABASE xactprod RENAME TO xactqa1;"
+psql --dbname=postgresql://"${NON_PROD_USERNAME}":"${NON_PROD_PASSWORD}"@${NON_PROD_HOST}:5432/xactdev -c "ALTER DATABASE ${TEMP_PROD_DB} RENAME TO xactqa1;"
 
 echo "Drop old database"
 psql --dbname=postgresql://"${NON_PROD_USERNAME}":"${NON_PROD_PASSWORD}"@${NON_PROD_HOST}:5432/xactqa -c "DROP DATABASE xactdev;"
