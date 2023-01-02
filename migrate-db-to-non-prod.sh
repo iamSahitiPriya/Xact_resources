5fb286a08aae23a351df9d48ae79ee512be222df
Shashank Mishra <shashank.mishra@thoughtworks.com>
2023-01-02 19:29:33 +0530

    [226] Shashank | Grant permissions to db connection user

--- migrate-db-to-non-prod.sh
+++ migrate-db-to-non-prod.sh
@@ -3,6 +3,7 @@ TEMP_PROD_USERNAME=$(aws secretsmanager get-secret-value --secret-id migration/d
 TEMP_PROD_PASSWORD=$(aws secretsmanager get-secret-value --secret-id migration/db --output text | grep -o '"password":"[^"]*' | grep -o '[^"]*$' | sed 's/!/\\!/g')
 NON_PROD_USERNAME=$(aws secretsmanager get-secret-value --secret-id non-prod/db --output text --query SecretString | grep -o '"username":"[^"]*' | grep -o '[^"]*$')
 NON_PROD_PASSWORD=$(aws secretsmanager get-secret-value --secret-id non-prod/db --output text --query SecretString | grep -o '"password":"[^"]*' | grep -o '[^"]*$')
+DB_CONNECT_USERNAME=$(aws secretsmanager get-secret-value --secret-id dev/dbuser --output text | grep -o '"DEV_DB_USER":"[^"]*' | grep -o '[^"]*$' | sed 's/!/\\!/g')
 TEMP_PROD_INSTANCE_NAME=temp-prod-instance
 TEMP_PROD_DB=xactprodtemp
 PROD_DB=xactprod
@@ -81,6 +82,12 @@ psql --dbname=postgresql://"${NON_PROD_USERNAME}":${NON_PROD_PASSWORD}@${NON_PRO
 echo "Rename dev1 and qa1"
 psql --dbname=postgresql://"${NON_PROD_USERNAME}":${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactqa1 -c "ALTER DATABASE xactdev1 RENAME TO xactdev;"
 psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev -c "ALTER DATABASE xactqa1 RENAME TO xactqa;"
+
+
+echo "Grant permissions"
+psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactdev -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "'${DB_CONNECT_USERNAME}'";'
+psql --dbname=postgresql://${NON_PROD_USERNAME}:${NON_PROD_PASSWORD}@${NON_PROD_HOST}:5432/xactqa -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "'${DB_CONNECT_USERNAME}'";'
+
 
 echo "Changing hosted zone"
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch "{\"Changes\": [{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"xact-db-np.xact.thoughtworks.net\",\"Type\":\"CNAME\",\"TTL\":30,\"ResourceRecords\":[{\"Value\":\"${NON_PROD_HOST}\"}]}}]}"
