15ec9219b49c0cae36581d51b66f9daf7e3614a6
Shashank Mishra <shashank.mishra@thoughtworks.com>
2023-02-27 11:15:54 +0530

    [No-Card] Shashank | Start sonar automatically

--- Common/Sonar/install_sonar.sh
+++ Common/Sonar/install_sonar.sh
@@ -7,5 +7,7 @@ sudo adduser --system --no-create-home --group --disabled-login sonarqube
 sudo mkdir /opt/sonarqube
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip
 sudo chown -R sonarqube:sonarqube /opt/sonarqube
+sudo chmod -R 775 /opt/sonarqube
 cd /opt/sonarqube/sonarqube-9.4.0.54424/bin/linux-x86-64
-sudo sh sonar.sh start
+sudo su sonarqube
+sh sonar.sh start
