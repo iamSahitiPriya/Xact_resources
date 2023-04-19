f739b9a703ae62cf9cf3ffa09a141ea675d7dbb1
Shankytw <100943198+Shankytw@users.noreply.github.com>
2023-04-19 17:05:14 +0530

    Update instance size

--- Common/Jenkins/jenkins_ec2.tf
+++ Common/Jenkins/jenkins_ec2.tf
@@ -81,7 +81,7 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "web" {
   ami             = data.aws_ami.amazon_linux.id
-  instance_type   = "c6a.large"
+  instance_type   = "t3.xlarge"
   key_name        = var.key_name
   security_groups = [aws_security_group.jenkins_sg.name]
   user_data       = "${file("install_jenkins.sh")}"
